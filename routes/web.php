5c33be48944ae0fca103081a3f15528ba0f1c6d9
Tara Matthew <tara@tutorful.co.uk>
2024-01-21 19:47:26 +0000

    Add routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('tasks');
-});
+Route::get('/', [TaskController::class, 'index'])->name('home');
+
+Route::resource('tasks', TaskController::class)->only(['store', 'update', 'destroy']);
